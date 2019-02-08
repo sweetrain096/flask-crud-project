101b9d2cd01e891e5e1699fa933ce888794b7c24
sweetrain096 <gpfhddl09@gmail.com>
2019-02-08 01:16:34 +0000

    flask init

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
+app = Flask(__name__)
+@app.route("/")
+def index():
+    return "hi"
+    
+    
+    
+    
+    
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="8080", debug=True)
