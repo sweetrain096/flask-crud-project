38b83b8a560955a2148c1edab049218d059c8dd0
sweetrain096 <gpfhddl09@gmail.com>
2019-02-08 01:22:03 +0000

    sqlalchemy, migration setting

--- app.py
+++ app.py
@@ -2,7 +2,18 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 
+# flask 및 sqlalchemy 설정
 app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///db_flask.sqlite3"
+app.config["SQLALCHEMY_TREACK_MODIFICATIONS"] = False
+
+# sqlalchemy 및 migration 초기화
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+
+
+
 @app.route("/")
 def index():
     return "hi"
